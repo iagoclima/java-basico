232ad6fb758d22b15018cdb0335e47b5b067665b
Iago Campanhol de Lima <148512483+iagoclima@users.noreply.github.com>
2024-09-23 20:56:10 -0300

    Create VariaveisPontoFlutuante.java

---
+++ aula11/VariaveisPontoFlutuante.java
@@ -0,0 +1,16 @@
+package com.iago.cursojava.aula11;
+
+public class VariaveisPontoFlutuante {
+
+	public static void main(String[] args) {
+
+		double valorDaPassagem = 2.90;
+		
+		float valorTomate = 3.95f;
+		
+		System.out.println("Valor da passagem: " + valorDaPassagem);
+		System.out.println("Valor do tomate: " + valorTomate);
+
+	}
+
+}
