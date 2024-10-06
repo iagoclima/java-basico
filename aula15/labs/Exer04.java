34b002b2508b09424bb2bfa2aa8b359129a6c179
Iago Campanhol de Lima <148512483+iagoclima@users.noreply.github.com>
2024-10-06 12:23:40 -0300

    Create Exer04.java

---
+++ aula15/labs/Exer04.java
@@ -0,0 +1,20 @@
+package com.iago.cursojava.aula15.labs;
+
+import java.util.Scanner;
+
+public class Exer04 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite uma letra:");
+        String letra = scan.next();
+
+        if(letra.equalsIgnoreCase("a") || letra.equalsIgnoreCase("e") || letra.equalsIgnoreCase("i") || letra.equalsIgnoreCase("o") || letra.equalsIgnoreCase("u")){
+            System.out.println("A letra digitada é uma vogal");
+        } else{
+            System.out.println("A letra digitada é uma consoante");
+        }
+    }
+}
