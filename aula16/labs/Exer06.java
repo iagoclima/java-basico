7912d75d32acbe0c45ad97d4e8f0f1e72ed8ca0c
Iago Campanhol de Lima <148512483+iagoclima@users.noreply.github.com>
2024-10-09 20:24:43 -0300

    Create Exer06.java

---
+++ aula16/labs/Exer06.java
@@ -0,0 +1,14 @@
+package com.iago.cursojava.aula17.labs;
+
+
+public class Exer06 {
+
+    public static void main(String[] args) {
+
+        int i;
+
+        for(i = 1; i <= 20; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
