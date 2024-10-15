402cbcd600c98103b11fd373993e8e5852555cb7
Iago Campanhol de Lima <148512483+iagoclima@users.noreply.github.com>
2024-10-15 19:36:13 -0300

    Create Livro.java

---
+++ aula24/labs/Livro.java
@@ -0,0 +1,11 @@
+package com.iago.cursojava.aula24.labs;
+
+public class Livro {
+
+    String titulo;
+    String autor;
+    int qtdPaginas;
+    String assunto;
+    int anoPublicado;
+    String sinopse;
+}
