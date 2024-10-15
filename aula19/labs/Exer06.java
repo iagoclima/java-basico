9a6d7cf8b512b0161f2e6caa9d8e9d5073cc2cd2
Iago Campanhol de Lima <148512483+iagoclima@users.noreply.github.com>
2024-10-15 14:28:36 -0300

    Create Exer06.java

---
+++ aula19/labs/Exer06.java
@@ -0,0 +1,33 @@
+package com.iago.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exer06 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[10];
+        int[] vetorC = new int[10];
+
+        for(int i=0; i<vetorA.length; i++){
+            System.out.println("Entre com o valor de A na posição: " + i);
+            vetorA[i] = scan.nextInt();
+        }
+
+        System.out.println();
+
+        for(int i=0; i<vetorB.length; i++){
+            System.out.println("Entre com o valor de B na posição: " + i);
+            vetorB[i] = scan.nextInt();
+        }
+
+        for(int i=0; i<vetorC.length; i++){
+            vetorC[i] = vetorA[i] + vetorB[i];
+            System.out.println("Vetor C na posição " + i + " = " + vetorC[i]);
+        }
+
+    }
+}
