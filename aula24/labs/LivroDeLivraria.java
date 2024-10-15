f7029644796889c6f7840137d7a9a48bea7ab309
Iago Campanhol de Lima <148512483+iagoclima@users.noreply.github.com>
2024-10-15 19:36:39 -0300

    Create LivroDeLivraria.java

---
+++ aula24/labs/LivroDeLivraria.java
@@ -0,0 +1,16 @@
+package com.iago.cursojava.aula24.labs;
+
+public class LivroDeLivraria {
+
+    public static void main(String[] args) {
+
+        Livro livro1 = new Livro();
+        livro1.titulo = "O Diário de Anne Frank";
+        livro1.autor = "Anne Frank";
+        livro1.qtdPaginas = 445;
+        livro1.assunto = "Guerra";
+        livro1.anoPublicado = 1947;
+        livro1.sinopse = "O Diário de Anne Frank é um livro escrito por Anne Frank entre 12 de junho de 1942 e 1.º de agosto de 1944 durante a Segunda Guerra Mundial. É conhecido por narrar momentos vivenciados pelo grupo de judeus confinados em um esconderijo durante a ocupação nazista dos Países Baixos.";
+
+    }
+}
