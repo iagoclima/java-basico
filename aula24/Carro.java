02439e0384f07a09ef4e74a1af95422733ccfdeb
Iago Campanhol de Lima <148512483+iagoclima@users.noreply.github.com>
2024-10-15 19:16:38 -0300

    Create Carro.java

---
+++ aula24/Carro.java
@@ -0,0 +1,10 @@
+package com.iago.cursojava.aula24;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+}
