4f5fbca0d33496c70804b1d0058acea066d80d5b
Iago Campanhol de Lima <148512483+iagoclima@users.noreply.github.com>
2024-10-15 19:21:07 -0300

    Create Lampada.java

---
+++ aula24/labs/Lampada.java
@@ -0,0 +1,9 @@
+package com.iago.cursojava.aula24.labs;
+
+public class Lampada {
+
+    String tipo;
+    int potenciaConsumida;
+    int durabilidade; //durabilidade em horas
+    double economiaEnergia;
+}
